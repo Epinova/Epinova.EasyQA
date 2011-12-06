e5813950f047ceb27205bc1ebffdd0e7b43dcac0
Arve Systad <arve@herlig.net>
2011-12-06 21:27:02 +0100

    More JS refactoring

--- Epinova.EasyQA/Scripts/common.js
+++ Epinova.EasyQA/Scripts/common.js
@@ -4,3 +4,31 @@
     $('#errorWindow').show();
     $('#errorWindow').fadeOut(7000);
 }
+
+var loadingClass = 'loading';
+function postAjax(url, data, elementToGetLoadingClass, successCallback) {
+    if (elementToGetLoadingClass) {
+        elementToGetLoadingClass.addClass(loadingClass);
+    }
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        contentType: 'application/json; charset=utf-8',
+        success: function (data) {
+            if (data.Error) {
+                displayError(data.Error);
+            }
+            if (elementToGetLoadingClass) {
+                elementToGetLoadingClass.removeClass(loadingClass);
+            }
+            if (typeof (successCallback) == 'function') {
+                successCallback(data);
+            }
+        },
+        error: function (data) {
+            displayError(data);
+            console.error(data);
+        }
+    });
+}
