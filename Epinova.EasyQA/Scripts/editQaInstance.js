0a45c698c680b3638a3d9a7884ec74eeea7a84c4
Arve Systad <arve@herlig.net>
2011-12-06 21:27:02 +0100

    More JS refactoring

--- Epinova.EasyQA/Scripts/editQaInstance.js
+++ Epinova.EasyQA/Scripts/editQaInstance.js
@@ -6,33 +6,6 @@
     var qaId = $('#qaId').val();
     var loadingClass = 'loading';
 
-    function postAjax(url, data, elementToGetLoadingClass, successCallback) {
-        if (elementToGetLoadingClass) {
-            elementToGetLoadingClass.addClass(loadingClass);
-        }
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
-            contentType: 'application/json; charset=utf-8',
-            success: function (data) {
-                if (data.Error) {
-                    displayError(data.Error);
-                }
-                if (elementToGetLoadingClass) {
-                    elementToGetLoadingClass.removeClass(loadingClass);
-                }
-                if (typeof (successCallback) == 'function') {
-                    successCallback(data);
-                }
-            },
-            error: function (data) {
-                displayError(data);
-                console.error(data);
-            }
-        });
-    }
-
     titleFieldLink.live('click', function (e) {
         e.preventDefault();
         var text = $(this).siblings('.content').text();
--- Epinova.EasyQA/Scripts/editQaType.js
+++ Epinova.EasyQA/Scripts/editQaType.js
@@ -2,7 +2,6 @@
     var qaTypeId = $('#qaTypeId').val();
     var sectionCountField = $('#sectionCount');
     var titleFieldLink = $('#title a');
-    var loadingClass = 'loading';
 
     $('input').live('keypress', function (e) {
         if (e.keyCode == 13)
@@ -18,79 +17,27 @@
     var editableTitle = $('.editableTitle');
     editableTitle.live('blur', function (e) {
         var editField = $(this);
-        editField.addClass(loadingClass);
         var text = $(this).val();
-        $.ajax({
-            url: '/QaType/UpdateQaTypeTitle/',
-            type: 'POST',
-            data: '{ "id": ' + qaTypeId + ', "text": "' + text + '" }',
-            contentType: 'application/json; charset=utf-8',
-            success: function (data) {
-                if (data.Error) {
-                    displayError(data.Error);
-                }
-                editField.parent().text(data.Title);
-                editField.remove();
-            },
-            error: function (data) {
-                displayError(data);
-            }
+        postAjax('/QaType/UpdateQaTypeTitle/', '{ "id": ' + qaTypeId + ', "text": "' + text + '" }', editField, function (data) {
+            editField.parent().text(data.Text);
+            editField.remove();
         });
     });
-
 
     var addCriteriaButtons = $('.addCriteria');
     var criteriaMarkup = '<li><span class="content">{text}</span><a href="#" class="editButton">(Endre)</a><input type="hidden" value="{id}" class="criteriaId"></li>';
     addCriteriaButtons.live('click', function (e) {
+        e.preventDefault();
         var clickedButton = $(this);
         $(clickedButton).siblings('.loader').show();
         var list = $(this).siblings('ol');
         var thisCriteriaCategory = $(this).closest('.editableCategory').children('.categoryId').val();
-        e.preventDefault();
-        $.ajax({
-            url: '/QaType/CreateCriteria/',
-            data: { qaType: qaTypeId, categoryId: thisCriteriaCategory },
-            type: 'POST',
-            success: function (data) {
-                if (data.Error) {
-                    displayError(data.Error);
-                }
-                var modifiedCriteriaMarkup = criteriaMarkup.replace('{id}', data.Id)
-                                                           .replace('{text}', data.Title);
-                $(list).append(modifiedCriteriaMarkup);
-                $(clickedButton).siblings('.loader').hide();
-            },
-            error: function (e) {
-                displayError(e);
-                console.log(e);
-            }
-        });
-    });
 
-    var addCategoryButtons = $('.addCategory');
-    var categoryMarkup = '<section class="editableCategory"><input type="hidden" value="{id}" class="categoryId"><h1><span class="sectionIndex">{index}</span> <span class="content">(uten navn)</span> <a href="#">(Endre)</a></h1><ol></ol><span class="button addItemButton addCriteria"><a href="#">Legg til nytt kriterie</a></span></section>';
-    addCategoryButtons.live('click', function (e) {
-        var clickedButton = $(this);
-        $(clickedButton).siblings('.loader').show();
-        e.preventDefault();
-        $.ajax({
-            url: '/QaType/CreateCategory/',
-            type: 'POST',
-            data: { qaType: qaTypeId },
-            success: function (data) {
-                if (data.Error) {
-                    displayError(data.Error);
-                }
-                sectionCountField.val(parseInt(sectionCountField.val()) + 1);
-                console.log("Section count = " + sectionCountField.val());
-                var modifiedcategoryMarkup = categoryMarkup.replace('{id}', data.Id)
-                                                           .replace('{index}', sectionCountField.val());
-                $('#sectionWrapper').append(modifiedcategoryMarkup);
-                $(clickedButton).siblings('.loader').hide();
-            },
-            error: function (data) {
-                console.log(data)
-            }
+        postAjax('/QaType/CreateCriteria/', '{ "qaType":' + qaTypeId + ', "categoryId":' + thisCriteriaCategory + '}', null, function (data) {
+            var modifiedCriteriaMarkup = criteriaMarkup.replace('{id}', data.Id)
+                                                       .replace('{text}', data.Text);
+            $(list).append(modifiedCriteriaMarkup);
+            $(clickedButton).siblings('.loader').hide();
         });
     });
 
@@ -106,25 +53,30 @@
     var editableCriteria = $('.editableCriteria');
     editableCriteria.live('blur', function (e) {
         e.preventDefault();
+        var clickedElement = $(this);
         var li = $(this).closest('li');
         var criteriaId = li.children('.criteriaId').val();
         var categoryId = $(this).closest('section.editableCategory').children('.categoryId').val();
-        console.log("criteriaId: " + criteriaId);
         var text = $(this).val();
-        $(this).addClass('loading');
-        $.ajax({
-            url: '/QaType/UpdateCriteriaText/',
-            data: { qaType: qaTypeId, criteriaId: criteriaId, text: text },
-            type: 'POST',
-            success: function (data) {
-                $(li).find('.editableCriteria').remove();
-                $(li).attr('value', data.Sortorder);
-                $(li).find('.content').text(data.Title);
-            },
-            error: function (e) {
-                displayError(e);
-                console.log(e);
-            }
+
+        postAjax('/QaType/UpdateCriteriaText/', '{ "qaType":' + qaTypeId + ', "criteriaId":' + criteriaId + ', "text": "' + text + '" }', clickedElement, function (data) {
+            $(li).find('.editableCriteria').remove();
+            $(li).find('.content').text(data.Text);
+        });
+    });
+
+    var addCategoryButtons = $('.addCategory');
+    var categoryMarkup = '<section class="editableCategory"><input type="hidden" value="{id}" class="categoryId"><h1><span class="sectionIndex">{index}</span> <span class="content">(uten navn)</span> <a href="#">(Endre)</a></h1><ol></ol><span class="button addItemButton addCriteria"><a href="#">Legg til nytt kriterie</a></span></section>';
+    addCategoryButtons.live('click', function (e) {
+        var clickedButton = $(this);
+        $(clickedButton).siblings('.loader').show();
+        e.preventDefault();
+        postAjax('/QaType/CreateCategory/', '{ "qaType":' + qaTypeId + '}', null, function (data) {
+            sectionCountField.val(parseInt(sectionCountField.val()) + 1);
+            var modifiedcategoryMarkup = categoryMarkup.replace('{id}', data.Id)
+                                                        .replace('{index}', sectionCountField.val());
+            $('#sectionWrapper').append(modifiedcategoryMarkup);
+            $(clickedButton).siblings('.loader').hide();
         });
     });
 
@@ -138,31 +90,15 @@
 
     var editableCategoryTitle = $('.editableCategoryTitle');
     editableCategoryTitle.live('blur', function (e) {
-        e.preventDefault();
         var h1field = $(this).closest('h1');
         var text = $(this).val();
         var categoryId = $(this).closest('section').find('.categoryId').val();
         if (categoryId == "")
             categoryId = 0;
-
-        $(this).addClass(loadingClass);
-        $.ajax({
-            url: '/QaType/UpdateCategoryTitle/',
-            data: { qaTypeId: qaTypeId, categoryId: categoryId, text: text },
-            type: 'POST',
-            success: function (data) {
-                $(this).removeClass(loadingClass);
-                if (data.Error) {
-                    displayError(data.Error);
-                    return;
-                }
-                $(h1field).find('.editableCategoryTitle').remove();
-                $(h1field).find('.content').text(data.Title);
-            },
-            error: function (e) {
-                displayError(e.d);
-                console.log(e);
-            }
+        var clickedElement = $(this);
+        postAjax('/QaType/UpdateCategoryTitle/', '{ "qaTypeId": ' + qaTypeId + ', "categoryId":' + categoryId + ', "text": "' + text + '" }', clickedElement, function (data) {
+            $(h1field).find('.editableCategoryTitle').remove();
+            $(h1field).find('.content').text(data.Text);
         });
     });
 })();
