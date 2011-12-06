08a6e3e93e7513278248f312d0661fc5de8ff447
Arve Systad <arve@herlig.net>
2011-12-06 23:09:32 +0100

    New file added

---
+++ Epinova.EasyQA.Tests/mocks/CategoryRepositoryMock.cs
@@ -0,0 +1,19 @@
+using System;
+using Epinova.EasyQA.Core.DataInterfaces;
+using Epinova.EasyQA.Core.Entities;
+
+namespace Epinova.EasyQA.Tests.Mocks
+{
+    public class CategoryRepositoryMock : IQaCategoryRepository
+    {
+        public CriteriaCategory CreateCriteriaCategory(int qaType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CriteriaCategory UpdateCriteriaCategory(int qaType, int categoryId, string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
