97b0eb481e2fdb08b1ec2c61dfc3309abb55968e
Arve Systad <arve@herlig.net>
2012-06-16 15:06:23 +0200

    added dice icons

---
+++ Epinova.EasyQA.Tests/HelperTests/QaHelperTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Core;
+using Epinova.EasyQA.Utilities;
+using Epinova.EasyQA.Core.Entities;
+using Epinova.EasyQA.Core;
+using Epinova.EasyQA.Tests.TestHelpers;
+
+namespace Epinova.EasyQA.Tests.HelperTests
+{
+    [TestFixture]
+    public class QaHelperTests
+    {
+        [Test]
+        public void GetScoreOutOfSix_50points_ScoreShouldBe3() 
+        {
+            var qaInstance = new QaInstance() { Score = 50 };
+            QaHelper.GetScoreOutOfSix(qaInstance).ShouldBe(3);
+        }
+
+        [Test]
+        public void GetScoreOutOfSix_80points_ScoreShouldBe5() 
+        {
+            var qaInstance = new QaInstance() { Score = 80 };
+            QaHelper.GetScoreOutOfSix(qaInstance).ShouldBe(5);
+        }
+
+        [Test]
+        public void GetScoreOutOfSix_20points_ScoreShouldBe1() 
+        {
+            var qaInstance = new QaInstance() { Score = 20 };
+            QaHelper.GetScoreOutOfSix(qaInstance).ShouldBe(1);
+        }
+
+        [Test]
+        public void GetScoreOutOfSix_90points_ScoreShouldBe6() 
+        {
+            var qaInstance = new QaInstance() { Score = 90 };
+            QaHelper.GetScoreOutOfSix(qaInstance).ShouldBe(6);
+        }
+    }
+}
---
+++ Epinova.EasyQA/Utilities/QaHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Epinova.EasyQA.Core.Entities;
+
+namespace Epinova.EasyQA.Utilities
+{
+    public static class QaHelper
+    {
+        public static int GetScoreOutOfSix(QaInstance qa)
+        {
+            if(qa.Score >= 90)
+                return 6;
+
+            double scoreOutOfSix = qa.Score / 16.6;
+            return (int)Math.Round(scoreOutOfSix, MidpointRounding.ToEven);
+        }
+    }
+}
